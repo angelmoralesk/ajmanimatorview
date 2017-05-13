804b1f29d976ebde631f7004e5074654fa72bd78
Angel Morales Karam <angelmorales@deloittemx.com>
2017-05-13 08:18:12 -0500

    Initial Commit

---
+++ AJMAnimatorView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AJMAnimatorView
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 13/05/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
